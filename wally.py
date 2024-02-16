c73138d7899a79b0f8daf4a98222e5784b1f7359
User <viktorianika28key@gmail.com>
2024-02-16 19:33:09 +0200

    add scale

--- wally.py
+++ wally.py
@@ -1,12 +1,18 @@
 from tkinter import *
 
 root=Tk()
-root.geometry('750x600')
+root.geometry('750x550')
 root.title('Viktoriia')
 root.iconbitmap('coffee.ico')
 
 img=PhotoImage(file='Wally.png')
 lb=Label(image=img)
 lb.pack()
+
+def set_gamma(val):
+    img['gamma']=val
+
+scale=Scale(from_=1, to=10, length=700, orient=HORIZONTAL, command=set_gamma)
+scale.pack()
 
 root.mainloop()

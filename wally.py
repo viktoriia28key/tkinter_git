f2bc210747ba334838c58a908d1392c08375168f
User <viktorianika28key@gmail.com>
2024-02-16 20:08:26 +0200

    Revert "add scale"
    This reverts commit c73138d7899a79b0f8daf4a98222e5784b1f7359.

--- wally.py
+++ wally.py
@@ -12,10 +12,4 @@ img=PhotoImage(file='Wally.png')
 lb=Label(image=img)
 lb.pack()
 
-def set_gamma(val):
-    img['gamma']=val
-
-scale=Scale(from_=1, to=10, length=700, orient=HORIZONTAL, command=set_gamma)
-scale.pack()
-
 root.mainloop()

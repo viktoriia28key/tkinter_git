478e975385a3e3699fe6e89446dcbec3b2ca27b5
User <viktorianika28key@gmail.com>
2024-02-16 20:10:46 +0200

    all

--- wally.py
+++ wally.py
@@ -1,9 +1,12 @@
 from tkinter import *
 
 root=Tk()
-root.geometry('750x550')
+root.geometry('750x600')
 root.title('Viktoriia')
 root.iconbitmap('coffee.ico')
+
+text=Label(text='Gamma editor', font='Arial 20')
+text.pack()
 
 img=PhotoImage(file='Wally.png')
 lb=Label(image=img)
